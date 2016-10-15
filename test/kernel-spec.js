c68ef2e440308dcc8473b9b4337e7bd7708e2947
Safia Abdalla <safia@safia.rocks>
2016-10-15 10:34:47 -0500

    fix(kernel): Kernel API should take parameters

--- test/kernel-spec.js
+++ test/kernel-spec.js
@@ -1,9 +1,12 @@
 import * as kernel from '../lib/kernel';
 import { expect } from 'chai';
 
+const endpoint = 'http://localhost:8888/api';
+const crossDomain = true;
+
 describe('createAllKernelSpec', () => {
   it('creates a payload for a kernelspec request', () => {
-    const request = kernel.createAllKernelSpec();
+    const request = kernel.createAllKernelSpec(endpoint, crossDomain);
 
     expect(request).to.deep.equal({
       url: 'http://localhost:8888/api/kernelspecs',
@@ -15,7 +18,7 @@ describe('createAllKernelSpec', () => {
 
 describe('createAllKernel', () => {
   it('creates a payload for a kernels request', () => {
-    const request = kernel.createAllKernel();
+    const request = kernel.createAllKernel(endpoint, crossDomain);
 
     expect(request).to.deep.equal({
       url: 'http://localhost:8888/api/kernels',
@@ -27,7 +30,7 @@ describe('createAllKernel', () => {
 
 describe('createKernel', () => {
   it('creates a payload for a single kernel request', () => {
-    const request = kernel.createKernel('test-id');
+    const request = kernel.createKernel(endpoint, crossDomain, 'test-id');
 
     expect(request).to.deep.equal({
       url: 'http://localhost:8888/api/kernels/test-id',
@@ -39,7 +42,7 @@ describe('createKernel', () => {
 
 describe('createLaunchKernel', () => {
   it('creates a payload for a launch kernel request', () => {
-    const request = kernel.createLaunchKernel('python3', '~');
+    const request = kernel.createLaunchKernel(endpoint, crossDomain, 'python3', '~');
 
     expect(request).to.deep.equal({
       url: 'http://localhost:8888/api/kernels',
