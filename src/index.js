5465f1c19356485d45286e21b9893ff886a8915c
Safia Abdalla <safia@safia.rocks>
2016-10-15 10:35:10 -0500

    feat(JupyterAPI): Add JupyterAPI prototype

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import * as kernels from './kernel.js';
+
+const JupyterAPI = function(endpoint, crossDomain) {
+  this.endpoint = endpoint;
+  this.crossDomain = crossDomain;
+}
+
+JupyterAPI.prototype.getAllKernelSpec$ = () => {
+  return kernels.getAllKernelSpec$.bind(null, this.endpoint, this.crossDomain);
+}
+JupyterAPI.prototype.getAllKernel$ = () => {
+  return kernels.getAllKernel$.bind(null, this.endpoint, this.crossDomain);
+}
+JupyterAPI.prototype.getKernel$ = () => {
+  return kernels.getKernel$.bind(null, this.endpoint, this.crossDomain);
+}
+JupyterAPI.prototype.launchKernel$ = () => {
+  return kernels.launchKernel$.bind(null, this.endpoint, this.crossDomain);
+}
+
+export { JupyterAPI };
