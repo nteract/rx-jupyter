30bc5767a95d651feb3b3311f319133df442c815
Kyle Kelley <rgbkrk@gmail.com>
2016-10-16 09:36:30 -0700

    chore(tests): form a test hierarchy for viz

--- test/kernel-spec.js
+++ test/kernel-spec.js
@@ -4,46 +4,57 @@ import { expect } from 'chai';
 const endpoint = 'http://localhost:8888';
 const crossDomain = true;
 
-describe('createSettingsForList', () => {
-  it('creates the AJAX settings for listing the kernels', () => {
-    const request = kernels.createSettingsForList(endpoint, crossDomain);
+describe('kernels', () => {
+  describe('createSettingsForList', () => {
+    it('creates the AJAX settings for listing the kernels', () => {
+      const request = kernels.createSettingsForList(endpoint, crossDomain);
 
-    expect(request).to.deep.equal({
-      url: 'http://localhost:8888/api/kernels',
-      crossDomain: true,
-      responseType: 'json',
+      expect(request).to.deep.equal({
+        url: 'http://localhost:8888/api/kernels',
+        crossDomain: true,
+        responseType: 'json',
+      });
     });
   });
-});
 
-describe('createSettingsForGet', () => {
-  it('creates the AJAX settings for getting a kernel by ID', () => {
-    const request = kernels.createSettingsForGet(endpoint, crossDomain, 'test-id');
+  describe('createSettingsForGet', () => {
+    it('creates the AJAX settings for getting a kernel by ID', () => {
+      const request = kernels.createSettingsForGet(endpoint, crossDomain, 'test-id');
 
-    expect(request).to.deep.equal({
-      url: 'http://localhost:8888/api/kernels/test-id',
-      crossDomain: true,
-      responseType: 'json',
+      expect(request).to.deep.equal({
+        url: 'http://localhost:8888/api/kernels/test-id',
+        crossDomain: true,
+        responseType: 'json',
+      });
     });
   });
-});
 
-describe('createSettingsForStart', () => {
-  it('creates the AJAX settings for creating a kernel', () => {
-    const request = kernels.createSettingsForStart(endpoint, crossDomain, 'python3', '~');
+  describe('createSettingsForStart', () => {
+    it('creates the AJAX settings for creating a kernel', () => {
+      const request = kernels.createSettingsForStart(endpoint, crossDomain, 'python3', '~');
 
-    expect(request).to.deep.equal({
-      url: 'http://localhost:8888/api/kernels',
-      crossDomain: true,
-      responseType: 'json',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: {
-        path: '~',
-        kernel_name: 'python3',
-      }
+      expect(request).to.deep.equal({
+        url: 'http://localhost:8888/api/kernels',
+        crossDomain: true,
+        responseType: 'json',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: {
+          path: '~',
+          kernel_name: 'python3',
+        }
+      });
     });
   });
+
+  describe('list', () => {
+    it('creates an AjaxObservable configured for listing', () => {
+      const kernel$ = kernels.list(endpoint, crossDomain);
+      const request = kernel$.request;
+      expect(request.url).to.equal(`${endpoint}/api/kernels`);
+      expect(request.method).to.equal("GET");
+    })
+  })
 });
--- test/kernelspecs-spec.js
+++ test/kernelspecs-spec.js
@@ -4,23 +4,25 @@ import { expect } from 'chai';
 const endpoint = 'http://localhost:8888';
 const crossDomain = true;
 
-describe('createSettingsForList', () => {
-  it('creates the AJAX settings for listing kernelspecs', () => {
-    const request = kernelspecs.createSettingsForList(endpoint, crossDomain);
+describe('kernelspecs', () => {
+  describe('createSettingsForList', () => {
+    it('creates the AJAX settings for listing kernelspecs', () => {
+      const request = kernelspecs.createSettingsForList(endpoint, crossDomain);
 
-    expect(request).to.deep.equal({
-      url: 'http://localhost:8888/api/kernelspecs',
-      crossDomain: true,
-      responseType: 'json',
+      expect(request).to.deep.equal({
+        url: 'http://localhost:8888/api/kernelspecs',
+        crossDomain: true,
+        responseType: 'json',
+      });
     });
   });
-});
 
-describe('list', () => {
-  it('creates an AjaxObservable configured for listing', () => {
-    const kernelSpec$ = kernelspecs.list(endpoint, crossDomain);
-    const request = kernelSpec$.request;
-    expect(request.url).to.equal(`${endpoint}/api/kernelspecs`);
-    expect(request.method).to.equal("GET");
+  describe('list', () => {
+    it('creates an AjaxObservable configured for listing', () => {
+      const kernelSpec$ = kernelspecs.list(endpoint, crossDomain);
+      const request = kernelSpec$.request;
+      expect(request.url).to.equal(`${endpoint}/api/kernelspecs`);
+      expect(request.method).to.equal("GET");
+    })
   })
 })
