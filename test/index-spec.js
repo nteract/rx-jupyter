2733914004c05551138048a183f2a10cd7d75310
Kyle Kelley <rgbkrk@gmail.com>
2016-10-17 10:50:15 -0700

    feat(version): observable for jupyter api version

--- test/index-spec.js
+++ test/index-spec.js
@@ -2,10 +2,22 @@ import { expect } from 'chai';
 
 import * as jupyter from '../src';
 
-// Mostly a dummy "have we exported all the things" test
 describe('rx-jupyter', () => {
+  // Mostly a dummy "have we exported all the things" test
   it('exports kernels and kernelspecs', () => {
     expect(jupyter.kernels).to.not.be.null; // eslint-disable-line no-unused-expressions
     expect(jupyter.kernelspecs).to.not.be.null; // eslint-disable-line no-unused-expressions
+  });
+
+  describe('apiVersion', () => {
+    it('creates an AjaxObservable for getting the notebook server version', () => {
+      const apiVersion$ = jupyter.apiVersion({
+        endpoint: 'https://somewhere.com',
+        crossDomain: true,
+      });
+      const request = apiVersion$.request;
+      expect(request.url).to.equal('https://somewhere.com/api');
+      expect(request.method).to.equal('GET');
+    });
   });
 });
