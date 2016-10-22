a519ce7d188942b51fb813cc882bc9aa48110bd9
John Detlefs <jdetle@gmail.com>
2016-10-22 16:23:04 -0700

    test(contents): test get, create, and remove

--- test/contents-spec.js
+++ test/contents-spec.js
@@ -20,8 +20,17 @@ describe('contents', () => {
     });
   });
 
+  describe('get', () => {
+    it('creates the AjaxObservable for getting content', () => {
+      const content$ = contents.get(serverConfig, '/walla/walla/bingbang.ipynb');
+      const request = content$.request;
+      expect(request.url).to.equal('http://localhost:8888/api/contents/walla/walla/bingbang.ipynb');
+      expect(request.method).to.equal('GET');
+    });
+  });
+
   describe('createSettingsForCreate', () => {
-    it('creates an AjaxObservable for creating content', () => {
+    it('creates the AJAX setting or creating content', () => {
       const model = {
         type: 'notebook',
         name: 'c.ipynb',
@@ -40,6 +49,43 @@ describe('contents', () => {
           'Content-Type': 'application/json',
         },
       });
+    });
+  });
+
+  describe('create', () => {
+    it('creates the AjaxObservable for creating content', () => {
+      const model = {
+        type: 'notebook',
+        name: 'c.ipynb',
+        writable: true,
+        content: {},
+        format: 'json',
+      };
+      const create$ = contents.create(serverConfig, '/a/b/c.ipynb', model);
+      const request = create$.request;
+      expect(request.url).to.equal('http://localhost:8888/api/contents/a/b/c.ipynb');
+      expect(request.method).to.equal('POST');
+    });
+  });
+
+  describe('createSettingsForRemove', () => {
+    it('creates the settings for the AJAX request to remove contents', () => {
+      const settings = contents.createSettingsForRemove(serverConfig, '/path.ipynb');
+      expect(settings).to.deep.equal({
+        url: 'http://localhost:8888/api/contents/path.ipynb',
+        crossDomain: true,
+        responseType: 'json',
+        method: 'DELETE',
+      });
+    });
+  });
+
+  describe('remove', () => {
+    it('creates the AjaxObservable for removing contents', () => {
+      const remove$ = contents.remove(serverConfig, '/path.ipynb');
+      const request = remove$.request;
+      expect(request.url).to.equal('http://localhost:8888/api/contents/path.ipynb');
+      expect(request.method).to.equal('DELETE');
     });
   });
 });
